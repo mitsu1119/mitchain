9a63bee03d8e983f42f9cce2323d38316c254fc9
mitsu1119 <hachimitsu1119@gmail.com>
2023-07-21 20:46:11 +0900

    test practice 7

--- test.py
+++ test.py
@@ -22,3 +22,31 @@ Fp = GF(97)
 assert Fp(95) * Fp(45) * Fp(31) == Fp(23)
 assert Fp(17) * Fp(13) * Fp(19) * Fp(44) == Fp(68)
 assert Fp(12)**7 * Fp(77)**49 == Fp(63)
+
+# test p11 (practice 5)
+print("<practice 5 p11>")
+Fp = GF(19)
+ks = [1,3,7,13,18]
+for k in ks:
+    print(f"k = {k} [", end="")
+    for i in range(19):
+        print(Fp(i) * Fp(k), end=" ")
+    print("]")
+
+# test p12
+Fp = GF(13)
+a = Fp(3)
+b = Fp(12)
+c = Fp(10)
+assert a * b == c
+
+# test p13 (practice 7)
+print()
+print("<practice 7 p13>")
+ps = [7, 11, 17, 31]
+for p in ps:
+    Fp = GF(p)
+    print(f"p = {p} [", end="")
+    for i in range(1, p):
+        print(Fp(i)**(p - 1), end=" ")
+    print("]")
