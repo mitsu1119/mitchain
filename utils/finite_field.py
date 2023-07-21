92a8f5d8b63c69e2861cf0d79d179684317f9e21
mitsu1119 <hachimitsu1119@gmail.com>
2023-07-21 20:21:28 +0900

    implemented FiniteField by inheritance via IntegerModRing

--- utils/finite_field.py
+++ utils/finite_field.py
@@ -1,86 +1,24 @@
+from utils.integer_mod_ring import *
+
 # --------------------------------------------------------------------------------------------
 # Factory Element
 # --------------------------------------------------------------------------------------------
-class FpElement:
-    def __init__(self, value: int, parent):
-        self.__parent = parent
-        self.__order = parent.order()
-        self.value = value % self.__order
-
-    # --------------------------------------------------------------------------------------------
-    # Calculatable Type
-    # --------------------------------------------------------------------------------------------
-    def is_calcable(self, other):
-        if isinstance(other, self.__class__):
-            if self.__parent == other.__parent:
-                return True
-        return False
-
-    # --------------------------------------------------------------------------------------------
-    # Arithmetic Operators
-    # --------------------------------------------------------------------------------------------
-    def __add__(self, other):
-        if self.is_calcable(other):
-            return FpElement((self.value + other.value) % self.__order, self.__parent)
-        raise TypeError(f"Unsupported operand for +: '{str(self.__parent)}' and '{str(other.__parent)}'")
-    def __sub__(self, other):
-        if self.is_calcable(other):
-            return FpElement((self.value - other.value) % self.__order, self.__parent)
-        raise TypeError(f"Unsupported operand for -: '{str(self.__parent)}' and '{str(other.__parent)}'")
-    def __mul__(self, other):
-        if self.is_calcable(other):
-            return FpElement((self.value * other.value) % self.__order, self.__parent)
-        raise TypeError(f"Unsupported operand for *: '{str(self.__parent)}' and '{str(other.__parent)}'")
+class FpElement(IntegerModRingElement):
     def __truediv__(self, other):
         if self.is_calcable(other):
             return self * other.inv()
-        raise TypeError(f"Unsupported operand for //: '{str(self.__parent)}' and '{str(other.__parent)}'")
+        raise TypeError(f"Unsupported operand for //: '{str(self.parent)}' and '{str(other.parent)}'")
     def inv(self):
-        return FpElement(pow(self.value, -1, self.__order), self.__parent)
-
-    # --------------------------------------------------------------------------------------------
-    # Comparison Operators
-    # --------------------------------------------------------------------------------------------
-    def __eq__(self, other):
-        if self.is_calcable(other):
-            return (self.value == other.value)
-        raise TypeError(f"Unsupported operand for ==: '{str(self.__parent)}' and '{str(other.__parent)}'")
-    
-    def __ne__(self, other):
-        if self.is_calcable(other):
-            return (self.value != other.value)
-        raise TypeError(f"Unsupported operand for !=: '{str(self.__parent)}' and '{str(other.__parent)}'")
-
-    # --------------------------------------------------------------------------------------------
-    # Other Special Method
-    # --------------------------------------------------------------------------------------------
-    def __str__(self):
-        return str(self.value)
+        return FpElement(pow(self.value, -1, self.order), self.parent)
 
 # --------------------------------------------------------------------------------------------
 # Type Factory
 # --------------------------------------------------------------------------------------------
-class FiniteField:
-    def __init__(self, order: int):
-        self.__order = order
-
-    def order(self):
-        return self.__order
-
+class FiniteField(IntegerModRing):
     def __call__(self, value):
         return FpElement(value, self)
 
-    def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            return (self.__order == other.__order)
-        return False
-    
-    def __ne__(self, other):
-        if isinstance(other, self.__class__):
-            return (self.__order != other.__order)
-        return True
-
     def __str__(self):
-        return f"finite field of order {self.__order}"
+        return f"finite field of order {self.order}"
 
 GF = FiniteField
--- utils/integer_mod_ring.py
+++ utils/integer_mod_ring.py
@@ -1,18 +1,18 @@
 # --------------------------------------------------------------------------------------------
 # Factory Element
 # --------------------------------------------------------------------------------------------
-class IntegerModRingElement:
+class IntegerModRingElement():
     def __init__(self, value: int, parent):
-        self.__parent = parent
-        self.__order = parent.order()
-        self.value = value % self.__order
+        self.parent = parent
+        self.order = parent.order
+        self.value = value % self.order
 
     # --------------------------------------------------------------------------------------------
     # Calculatable Type
     # --------------------------------------------------------------------------------------------
     def is_calcable(self, other):
         if isinstance(other, self.__class__):
-            if self.__parent == other.__parent:
+            if self.parent == other.parent:
                 return True
         return False
 
@@ -21,16 +21,16 @@ class IntegerModRingElement:
     # --------------------------------------------------------------------------------------------
     def __add__(self, other):
         if self.is_calcable(other):
-            return IntegerModRingElement((self.value + other.value) % self.__order, self.__parent)
-        raise TypeError(f"Unsupported operand for +: '{str(self.__parent)}' and '{str(other.__parent)}'")
+            return self.__class__((self.value + other.value) % self.order, self.parent)
+        raise TypeError(f"Unsupported operand for +: '{str(self.parent)}' and '{str(other.parent)}'")
     def __sub__(self, other):
         if self.is_calcable(other):
-            return IntegerModRingElement((self.value - other.value) % self.__order, self.__parent)
-        raise TypeError(f"Unsupported operand for -: '{str(self.__parent)}' and '{str(other.__parent)}'")
+            return self.__class__((self.value - other.value) % self.order, self.parent)
+        raise TypeError(f"Unsupported operand for -: '{str(self.parent)}' and '{str(other.parent)}'")
     def __mul__(self, other):
         if self.is_calcable(other):
-            return IntegerModRingElement((self.value * other.value) % self.__order, self.__parent)
-        raise TypeError(f"Unsupported operand for *: '{str(self.__parent)}' and '{str(other.__parent)}'")
+            return self.__class__((self.value * other.value) % self.order, self.parent)
+        raise TypeError(f"Unsupported operand for *: '{str(self.parent)}' and '{str(other.parent)}'")
 
     # --------------------------------------------------------------------------------------------
     # Comparison Operators
@@ -38,12 +38,12 @@ class IntegerModRingElement:
     def __eq__(self, other):
         if self.is_calcable(other):
             return (self.value == other.value)
-        raise TypeError(f"Unsupported operand for ==: '{str(self.__parent)}' and '{str(other.__parent)}'")
+        raise TypeError(f"Unsupported operand for ==: '{str(self.parent)}' and '{str(other.parent)}'")
     
     def __ne__(self, other):
         if self.is_calcable(other):
             return (self.value != other.value)
-        raise TypeError(f"Unsupported operand for !=: '{str(self.__parent)}' and '{str(other.__parent)}'")
+        raise TypeError(f"Unsupported operand for !=: '{str(self.parent)}' and '{str(other.parent)}'")
 
     # --------------------------------------------------------------------------------------------
     # Other Special Method
@@ -54,27 +54,24 @@ class IntegerModRingElement:
 # --------------------------------------------------------------------------------------------
 # Type Factory
 # --------------------------------------------------------------------------------------------
-class IntegerModRing:
+class IntegerModRing():
     def __init__(self, order: int):
-        self.__order = order
-
-    def order(self):
-        return self.__order
+        self.order = order
 
     def __call__(self, value):
         return IntegerModRingElement(value, self)
 
     def __eq__(self, other):
         if isinstance(other, self.__class__):
-            return (self.__order == other.__order)
+            return (self.order == other.order)
         return False
     
     def __ne__(self, other):
         if isinstance(other, self.__class__):
-            return (self.__order != other.__order)
+            return (self.order != other.order)
         return True
 
     def __str__(self):
-        return f"ring of integers modulo {self.__order}"
+        return f"ring of integers modulo {self.order}"
 
 Zmod = IntegerModRing
